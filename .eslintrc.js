e02845fc5de26f802f286af1577b4a019fb00b99
dhtrinh02 <dhtrinh02@gmail.com>
2025-07-01 18:31:53 -0700

    scrape and validate first 100 new post sorted by time.

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+export default {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Add any ESLint rules here
+  },
+};
